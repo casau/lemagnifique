c052999dfbe2dafd0b3fe23fab7ba6b1ea164be6
al1 <al1@localhost.localdomain>
2020-04-26 18:43:01 +0200

    Page query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,41 @@
-import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+import React from "react";
+// import { Link } from "gatsby";
+// import Header from "../components/header";
+import Layout from "../components/layout"
 
-export default () => (
-  <div style={{ color: `purple` }}>
+export default ({ data }) => (
+  /*   <div style={{ color: `purple` }}>
     <Link to="/contact/">Contact</Link>
     <Header headerText="Hello Gatsby!" />
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
+ */
+  /*   <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </div>
+ */
+  <Layout>
+    <h1>{data.site.siteMetadata.title}</h1>
+    <div>
+      <img
+        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+        alt="Group of pandas eating bamboo"
+      />
+    </div>
+  </Layout>
 )
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
